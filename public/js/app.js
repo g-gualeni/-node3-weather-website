024e91c7ad8071a2adfac699ad54805875ce4245
Gualeni <gianbattista.gualeni@datalogic.com>
2020-10-29 12:49:09 +0100

    Setup app for Heroku deployment

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,19 @@
 console.log('client side file is loaded')
 
 const weatherForm = document.querySelector('form')
-const searc = document.querySelector('input')
+const search = document.querySelector('input')
 const message_1 = document.querySelector('#message-1')
 const message_2 = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     
+    const location = search.value 
+    
     message_1.textContent = 'waiting...'
     message_2.textContent = ''
 
-    queryString = 'weather?address=' + searc.value
-    fetch(queryString).then((response) => {
+    fetch('weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error)    
